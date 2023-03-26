377698c38da9c7a2081c5a9d337f26d96db5707a
Facundo <facufaccioli18@gmail.com>
2023-03-26 11:58:47 -0300

    fetch data from tmdb api

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,31 @@
-export default function Home() {
-  return <h1 className='text-3xl font-bold'>HOME</h1>;
+import Results from '@/components/Results';
+
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || 'fetchTrending';
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === 'fetchTopRated' ? 'movie/top_rated' : 'trending/all/week'
+    }?api_key=${API_KEY}&lenguage=en-US&page=1`,
+    {
+      next: {
+        revalidate: 10000,
+      },
+    },
+  );
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  const data = await res.json();
+  const results = data.results;
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
