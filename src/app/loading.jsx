ae2218ade7487202c20ecb7dfe3cbc633240a77a
Facundo <facufaccioli18@gmail.com>
2023-03-26 15:56:56 -0300

    add loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className='flex justify-center'>
+      <img className='h-96' src='spinner.svg' alt='Loading...' />
+    </div>
+  );
+}
+
+export default Loading;
