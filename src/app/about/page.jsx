41a54e060129c56cd11efb74d003e75b65abe626
Facundo <facufaccioli18@gmail.com>
2023-03-25 22:57:52 -0300

    create navbar component

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function About() {
   return (
-    <div className='max-w-6xl mx-4 space-y-4 text-justify lg:mx-auto'>
+    <div className='max-w-6xl p-4 mx-auto space-y-4 text-justify lg:p-6'>
       <h1 className='text-2xl font-medium text-amber-600'>About</h1>
       <p>
         Welcome to our movie database website! We are a team of passionate movie
