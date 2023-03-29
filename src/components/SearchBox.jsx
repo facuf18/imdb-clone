6444c2e3b7c30461d13002a2a0a70c88ab608626
Facundo <facufaccioli18@gmail.com>
2023-03-29 18:19:04 -0300

    add search ability

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+function SearchBox() {
+  const [search, setSearch] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    router.push(`/search/${search}`);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className='flex items-center justify-between max-w-xl px-5 mx-auto'
+    >
+      <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        type='text'
+        placeholder='Search keywords...'
+        className='flex-1 w-full placeholder-gray-500 bg-transparent rounded-sm outline-none h-14'
+      />
+      <button
+        disabled={!search}
+        type='submit'
+        className='text-amber-600 disabled:text-gray-400 '
+      >
+        Search
+      </button>
+    </form>
+  );
+}
+
+export default SearchBox;
